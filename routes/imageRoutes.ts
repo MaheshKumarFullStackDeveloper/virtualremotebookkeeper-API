1bff9708f7ed887b48d73aa372960e936a9b6924
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-06-25 16:17:03 +0530

    mahesh 48 commit

--- routes/imageRoutes.ts
+++ routes/imageRoutes.ts
@@ -4,10 +4,10 @@ import { multerMiddleware } from "../config/cloudnaryConfig";
 import * as imageController from "../controllers/imageController";
 const router = Router();
 
-//router.post("/",authenticatedUser,multerMiddleware,imageController.uploadPhoto);
+router.post("/", authenticatedUser, multerMiddleware, imageController.uploadPhoto);
 router.get("/:page/:limit", imageController.getAllImages);
 router.delete("/:imageId", authenticatedUser, imageController.deleteImage);
-//router.get("/user/:userId", imageController.getImagesByUserId);
+router.get("/user/:userId", imageController.getImagesByUserId);
 export default router;
 
 
