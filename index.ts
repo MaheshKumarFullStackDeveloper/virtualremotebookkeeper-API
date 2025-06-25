2af479dfb6b18cf3d40312173bd686af3285e1d6
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-06-25 12:32:36 +0530

    mahesh 14 commit

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
-
+import dotenv from 'dotenv';
 const express = require('express');
 const app = express();
 
-app.get('/', (req: Request, res: Response) => { res.send('Hello from Express!') });
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req: Request, res: Response) => { res.send(`Hello from Express! ${PORT}`) });
 
 module.exports = app; // ✅ Don't call app.listen()
