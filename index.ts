ec129a953ed60c0cd2ab5cffc388d4462bb3ac5c
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-06-25 12:59:59 +0530

    mahesh 28 commit

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-
-import express, { Request, Response, NextFunction } from "express";
-
 import dotenv from 'dotenv';
+
+import { Request, Response, NextFunction } from "express";
+
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookiesParser from 'cookie-parser';
 import connectDb from './config/dbConnect';
 import authRoutes from './routes/authRoutes';
+import productRoutes from './routes/productRoutes';
 import imageRoutes from './routes/imageRoutes';
 import userRoutes from './routes/userRoutes';
 import pageRoutes from './routes/pageRoutes';
@@ -17,36 +18,20 @@ import faqRoutes from './routes/faqRoutes';
 import categoryRoutes from './routes/categoryRoutes';
 import menuRoutes from './routes/menuRoutes';
 import widgetRoutes from './routes/widgetRoutes';
-
+const express = require('express');
+const app = express();
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
-
-const app = express();
 const corsOption = {
-    /*  origin: process.env.FRONT_URL, */
     origin: '*',
     credentials: true
 }
 
-
-
-
-// Middleware to handle CORS errors
-const corsErrorHandler = (req: Request, res: Response, next: NextFunction): void => {
-    const allowedOrigins = [process.env.FRONT_URL];
-    if (!allowedOrigins.includes(req.headers.origin as string)) {
-        res.status(403).json({ error: "CORS error: Origin not allowed" });
-    } else {
-        next();
-    }
-};
-
-
-
 app.use(cors(corsOption));
-//app.use(corsErrorHandler);
+
+const PORT = process.env.PORT || 8080;
+
 
 
 
@@ -54,15 +39,9 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookiesParser());
 
-
-/* app.listen(PORT, () => {
-    console.log(`lission port ${PORT}`);
-
-}) */
-
-
 connectDb();
 
+app.get('/', (req: Request, res: Response) => { res.send(`Hello - 1 from Express! ${PORT}`) });
 
 
 app.use("/api/auth", authRoutes)
@@ -70,15 +49,12 @@ app.use("/api/auth", authRoutes)
 app.use("/api/page", pageRoutes)
 app.use("/api/category", categoryRoutes)
 app.use("/api/blog", blogRoutes)
-app.use("/api/faqcategory", faqcategoryRoutes)
+/* app.use("/api/faqcategory", faqcategoryRoutes)
 app.use("/api/faq", faqRoutes)
 app.use("/api/section", sectionRoutes)
 app.use("/api/widget", widgetRoutes)
 app.use("/api/image", imageRoutes)
 app.use("/api/navigation", menuRoutes)
-app.use("/api/user/profile", userRoutes)
+app.use("/api/user/profile", userRoutes) */
 
-app.get('/', (req, res) => { res.send('Hello 1 from Express!') });
-
-
-module.exports = app;
+module.exports = app; // ✅ Don't call app.listen()
