ad3b677aae08f41a3cb2f0505c133a8acb2a7038
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-06-25 16:25:46 +0530

    mahesh 50 commit

--- index.ts
+++ index.ts
@@ -69,7 +69,7 @@ connectDb();
 
 
 app.use("/api/auth", authRoutes)
-app.use("/api/product", productRoutes)
+//app.use("/api/product", productRoutes)
 app.use("/api/page", pageRoutes)
 app.use("/api/category", categoryRoutes)
 app.use("/api/blog", blogRoutes)
