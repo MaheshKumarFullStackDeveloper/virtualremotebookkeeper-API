016aec073ad276d76c8d4b3ad4281cb07357568e
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-06-25 12:36:35 +0530

    mahesh 15 commit

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
-import { Request, Response } from "express";
 import dotenv from 'dotenv';
+
+import { Request, Response, NextFunction } from "express";
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookiesParser from 'cookie-parser';
+import connectDb from './config/dbConnect';
+import authRoutes from './routes/authRoutes';
+import productRoutes from './routes/productRoutes';
+import imageRoutes from './routes/imageRoutes';
+import userRoutes from './routes/userRoutes';
+import pageRoutes from './routes/pageRoutes';
+import sectionRoutes from './routes/sectionRoutes';
+import blogRoutes from './routes/blogRoutes';
+import faqcategoryRoutes from './routes/faqcategoryRoutes';
+import faqRoutes from './routes/faqRoutes';
+import categoryRoutes from './routes/categoryRoutes';
+import menuRoutes from './routes/menuRoutes';
+import widgetRoutes from './routes/widgetRoutes';
 const express = require('express');
 const app = express();
 
