6d5fdefbcced12a1a0d6f22b44c06bea8a5d10d0
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-07-04 19:11:13 +0530

    19 commit

--- index.ts
+++ index.ts
@@ -66,7 +66,7 @@ app.use(corsErrorHandler);
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 500, // Limit each IP to 100 requests per windowMs
     message: "Too many requests, please try again later.",
 });
 app.use(limiter);
